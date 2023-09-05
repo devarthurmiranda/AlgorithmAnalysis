69fe2023423eb013b2e1a8259793b040e2441ad2
Arthur Miranda <arthurfmbm12@hotmail.com>
2023-09-05 11:24:07 -0300

    bubble implementation

--- BubbleSort/Client.java
+++ BubbleSort/Client.java
@@ -12,11 +12,36 @@ public class Client {
         int swaps = 0;
         int n = array.length;
         int temp = 0;
+        boolean swapped = false;
 
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                comparisons++;
+                if (array[j] > array[j + 1]) {
+                    swaps++;
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
     }
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         int[] array = ArchiveOpener.openTenThousand();
         sort(array);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time elapsed: " + (endTime - startTime) + "ms");
     }
 }
