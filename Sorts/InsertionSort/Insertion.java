644dafa57e3242f6e3dec0e6d00bc56290ad40ac
Arthur Miranda <arthurfmbm12@hotmail.com>
2023-09-06 12:38:47 -0300

    Enchancing Insertion Description

--- Sorts/InsertionSort/Insertion.java
+++ Sorts/InsertionSort/Insertion.java
@@ -8,16 +8,18 @@ public class Insertion {
         int n = array.length;
         int temp = 0;
         int j = 0;
-        for(int i = 1; i < n; i++){
-            temp = array[i];
+        for(int i = 1; i < n; i++){ // Iterates through the array, starting at the second element
+
+            temp = array[i]; // Stores the current element in a temporary variable
+            
             j = i - 1;
-            while(j >= 0 && array[j] > temp){
+            while(j >= 0 && array[j] > temp){ // While the previous element is greater than the element on temporary variable
                 comparisons++;
                 swaps++;
-                array[j + 1] = array[j];
-                j--;
+                array[j + 1] = array[j]; // Set the current element to the previous element
+                j--; // Look at the previous position
             }
-            array[j + 1] = temp;
+            array[j + 1] = temp; // Set the current position the value of the temporary variable
         }
         System.out.println("Comparisons: " + comparisons);
         System.out.println("Swaps: " + swaps);
---
+++ Sorts/InsertionSort/NOTES.md
@@ -0,0 +1,11 @@
+## Insertion Sort
+
+### Description
+Insertion sort compares the current element with the element before it and swaps them if the element before is bigger, then moves on to the next element of the iteration and repeats the process.
+
+
+### Pros
+Simple algorithm to implement, stable, efficient when small number of elements are partially sorted.
+
+### Cons
+Inefficient when array is large and unsorted, inefficient when array is sorted in reverse order, high time complexity.
